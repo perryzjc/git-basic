8e37f3fbaaf1cee0b01f0a1bd9d6690a5a4800ef
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-22 02:29:26 -0800

    make checkFileExists static

--- src/main/java/GitBasic/Utils.java
+++ src/main/java/GitBasic/Utils.java
@@ -245,7 +245,7 @@ public class Utils {
         return dir.delete(); // The directory is empty now and can be deleted.
     }
 
-    public void checkFileExists(File file) {
+    public static void checkFileExists(File file) {
         if (!file.exists()) {
             throw new GitBasicException("File does not exist.");
         }
