62f99712f888313c150df6579a6f71429b8c6674
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-21 23:54:19 -0800

    modify BlobTest structure, change BeforeAll to Before Each

--- src/test/java/BlobTest.java
+++ src/test/java/BlobTest.java
@@ -1,9 +1,7 @@
 import GitBasic.FileStructure.FileStructure;
 import GitBasic.GitObject.Blob;
 import GitBasic.Utils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 
@@ -17,14 +15,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class BlobTest {
     private static File testFile;
-    @BeforeAll
-    public static void createTempDirAndFile() {
+    @BeforeEach
+    public void createTempDirAndFile() {
         FileStructure.initGitBasicDirectory();
         testFile = Utils.join("src", "test", "resources", "test_files", "a.txt");
     }
 
-    @AfterAll
-    public static void deleteTempDir() {
+    @AfterEach
+    public void deleteTempDir() {
         Utils.deleteDir(FileStructure.GIT_BASIC_DIR);
     }
 
@@ -36,6 +34,6 @@ public class BlobTest {
         Blob blob = new Blob(testFile);
         blob.serialize();
         files = FileStructure.BLOB_DIR.listFiles();
-        assertTrue(files != null && files.length > 0);
+        assertTrue(files != null && files.length == 1);
     }
 }
