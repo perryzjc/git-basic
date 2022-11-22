8d46f75a76a2cc866fc90c08df540bbffedb8dcb
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-22 06:10:41 -0800

    add deleteTempDir after each test

--- src/test/java/StagingAreaTest.java
+++ src/test/java/StagingAreaTest.java
@@ -1,6 +1,7 @@
 import GitBasic.FileStructure.FileStructure;
 import GitBasic.GitObject.StagingArea;
 import GitBasic.Utils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,10 +27,31 @@ public class StagingAreaTest {
         stagingArea = StagingArea.load();
     }
 
+    @AfterEach
+    public void deleteTempDir() {
+        Utils.deleteDir(FileStructure.GIT_BASIC_DIR);
+    }
+
     @Test
-    public void testStageForAddition() {
+    public void testStageForAddition1() {
         assertEquals(0, stagingArea.getFileAdded().size());
         stagingArea.stageForAddition(testWorkingFile);
         assertEquals(1, stagingArea.getFileAdded().size());
+    }
+
+    @Test
+    public void testStageForAddition2_repeatedFileAtCurrentStagingArea() {
+        assertEquals(0, stagingArea.getFileAdded().size());
+        stagingArea.stageForAddition(testWorkingFile);
+        assertEquals(1, stagingArea.getFileAdded().size());
+        stagingArea.stageForAddition(testWorkingFile);
+        assertEquals(1, stagingArea.getFileAdded().size());
+    }
+
+    @Test
+    /**
+     * TODO: need to implement after the commit class is done
+     */
+    public void testStageForAddition3_repeatedFileAtCurrenCommit() {
     }
 }
