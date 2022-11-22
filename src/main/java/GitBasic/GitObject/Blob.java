f95b81da96011b8b742013568d27cb7b0082c255
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-21 23:48:02 -0800

    modify Blob class and add test files, currently not passing the test

--- src/main/java/GitBasic/GitObject/Blob.java
+++ src/main/java/GitBasic/GitObject/Blob.java
@@ -1,5 +1,9 @@
 package GitBasic.GitObject;
 
+import GitBasic.GitBasicException;
+
+import java.io.File;
+
 /**
  * Bulb object stores the content of a file and its relative path at the working directory.
  * The filename of the blob file is the SHA-1 hash of the content of the file.
@@ -10,8 +14,27 @@ public class Blob implements GitObject {
     /* the relative path of the file at the working directory*/
     private String _filePath;
 
+    public Blob(File file) {
+        if (!file.exists()) {
+            throw new GitBasicException("File does not exist. Failed to create blob.");
+        }
+    }
+
     @Override
     public void serialize() {
+        return;
+    }
 
+    @Override
+    public GitObject deserialize(File file) {
+        return null;
+    }
+
+    public byte[] getData() {
+        return _data;
+    }
+
+    public String getFilePath() {
+        return _filePath;
     }
 }
--- src/test/java/BlobTest.java
+++ src/test/java/BlobTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class BlobTest {
+import GitBasic.FileStructure.FileStructure;
+import GitBasic.GitObject.Blob;
+import GitBasic.Utils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Test for the Blob class.
+ * Test whether blob can serialize a file at the correct path.
+ * Test whether blob can deserialize a file and return the original file (content and path)
+ */
+public class BlobTest {
+    private static File testFile;
+    @BeforeAll
+    public static void createTempDirAndFile() {
+        FileStructure.initGitBasicDirectory();
+        testFile = Utils.join("src", "test", "resources", "test_files", "a.txt");
+    }
+
+    @AfterAll
+    public static void deleteTempDir() {
+        Utils.deleteDir(FileStructure.GIT_BASIC_DIR);
+    }
+
+    @Test
+    public void testSerialize() {
+        File[] files;
+        files = FileStructure.BLOB_DIR.listFiles();
+        assertTrue(files != null && files.length == 0);
+        Blob blob = new Blob(testFile);
+        blob.serialize();
+        files = FileStructure.BLOB_DIR.listFiles();
+        assertTrue(files != null && files.length > 0);
+    }
 }
