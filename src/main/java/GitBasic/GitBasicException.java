d73fd35298b375b9765670cfac1c7df83804493f
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-21 22:42:45 -0800

    add customized exception for git-basic

---
+++ src/main/java/GitBasic/GitBasicException.java
@@ -0,0 +1,10 @@
+package GitBasic;
+
+/**
+ * General exception indicating git-basic error.
+ */
+public class GitBasicException extends RuntimeException {
+    public GitBasicException(String message) {
+        super(message);
+    }
+}
