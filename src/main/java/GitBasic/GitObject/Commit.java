d15a249c1f91be9b73639646898006b6d493ddf7
Jingchao Zhong <perry.zjc@gmail.com>
2022-11-22 01:25:27 -0800

    add commit class and CommitTest. Currently failed the test

--- src/main/java/GitBasic/GitObject/Commit.java
+++ src/main/java/GitBasic/GitObject/Commit.java
@@ -1,2 +1,57 @@
-package GitBasic.GitObject;public class Commit {
+package GitBasic.GitObject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Commit implements  GitObject{
+    /* the comment for the commit */
+    private String _message;
+    /* commit id, sha-1 hashed */
+    private String _commitId;
+    /* the parent commit ids, marge can cause multiple parent ids */
+    private ArrayList<String> _parentCommitIds;
+    /* the time stamp when the commit object is created */
+    private String _timeStamp;
+    /* key: file path, value: blob id */
+    private HashMap<String, String> _fileMap;
+
+    @Override
+    public void serialize() {
+
+    }
+
+    @Override
+    public void deserialize(File file) {
+
+    }
+
+    public void addFile(File file) {
+
+    }
+
+    public void addMessage(String message) {
+
+    }
+
+    public String getMessage() {
+        return _message;
+    }
+
+    public String getCommitId() {
+        return _commitId;
+    }
+
+    public ArrayList<String> getParentCommitIds() {
+        return _parentCommitIds;
+    }
+
+    public String getTimeStamp() {
+        return _timeStamp;
+    }
+
+    public HashSet<String> getBlobIds() {
+        return new HashSet<>(_fileMap.values());
+    }
 }
--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -1,2 +1,103 @@
-package PACKAGE_NAME;public class CommitTest {
+import GitBasic.GitBasicException;
+import GitBasic.GitObject.Commit;
+import GitBasic.FileStructure.FileStructure;
+import java.io.File;
+import GitBasic.Utils;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Test the commit class
+ * Check if the commit object file is created at the correct location
+ * Check if the commit object can recover the file stored in the blob object
+ */
+public class CommitTest {
+    private File testCommitFile;
+    private File testBlobFile;
+
+    @BeforeEach
+    public void createTempDir() {
+        FileStructure.initGitBasicDirectory();
+    }
+
+    @BeforeEach
+    public void createTempBlobFile() {
+        testBlobFile = new File(FileStructure.BLOB_DIR, "blob_test");
+        Utils.writeContents(testBlobFile, "blob_test");
+    }
+
+    @BeforeEach
+    public void createTempCommitFile() {
+        testCommitFile = new File(FileStructure.COMMIT_DIR, "commit_test");
+        Utils.writeContents(testCommitFile, "commit_test");
+    }
+
+    @AfterEach
+    public void deleteTempDir() {
+        assertTrue(Utils.deleteDir(FileStructure.GIT_BASIC_DIR), "Failed to delete temp dir");
+    }
+
+    @Test
+    public void testSerialize1_nothingToCommitException() {
+        Commit commit = new Commit();
+        assertThrows(GitBasicException.class, commit::serialize, "nothing to commit");
+    }
+
+    @Test
+    public void testSerialize2_noMessageException() {
+        Commit commit = new Commit();
+        commit.addFile(testBlobFile);
+        assertThrows(GitBasicException.class, commit::serialize, "commit need a message");
+    }
+
+    @Test
+    public void testSerialize3_commitFileCreated() {
+        File commitFile;
+        Commit commit = new Commit();
+        commitFile = new File(FileStructure.COMMIT_DIR, commit.getCommitId());
+        assertFalse(commitFile.exists());
+        commit.addFile(testBlobFile);
+        commit.serialize();
+        commitFile = new File(FileStructure.COMMIT_DIR, commit.getCommitId());
+        assertTrue(commitFile.exists(), "Commit file is not created successfully");
+    }
+
+    @Test
+    public void testDeserialize1_commitFileNotFound() {
+        Commit commit = new Commit();
+        assertThrows(GitBasicException.class, () -> commit.deserialize("no_exist_commit"), "commit file not found");
+    }
+
+    @Test
+    public void testDeserialize2_commitTimeStamp() {
+        Commit commit = new Commit();
+        commit.addFile(testBlobFile);
+        commit.serialize();
+        Commit commit2 = new Commit();
+        commit2.deserialize(commit.getCommitId());
+        assertEquals(commit.getTimeStamp(), commit2.getTimeStamp());
+    }
+
+    @Test
+    public void testDeserialize3_commitMessage() {
+        Commit commit = new Commit();
+        commit.addMessage("test message");
+        commit.addFile(testBlobFile);
+        commit.serialize();
+        Commit commit2 = new Commit();
+        commit2.deserialize(commit.getCommitId());
+        assertEquals("test message", commit2.getMessage());
+    }
+
+    @Test
+    public void testDeserialize4_checkBlobExists() {
+        Commit commit = new Commit();
+        commit.addFile(testBlobFile);
+        commit.serialize();
+        Commit commit2 = new Commit();
+        commit2.deserialize(commit.getCommitId());
+        File blobFile = new File(FileStructure.BLOB_DIR, commit2.getBlobIds().iterator().next());
+        assertTrue(blobFile.exists(), "Blob file does not exist");
+    }
 }
